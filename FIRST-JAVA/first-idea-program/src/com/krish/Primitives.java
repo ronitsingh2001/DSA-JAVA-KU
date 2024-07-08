801e1ccf79d1a6b9c70e5105f4d25df6a4c42fc3
Ronit <ronit2001krish@gmail.com>
2024-07-08 23:06:58 +0530

    Java program

---
+++ FIRST-JAVA/first-idea-program/src/com/krish/Primitives.java
@@ -0,0 +1,10 @@
+package com.krish;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'k';
+        float marks = 98.67f;
+        
+    }
+}
