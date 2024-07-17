583c273ddecdc3c9ad2427d109a494ea173c175c
Ronit <ronit2001krish@gmail.com>
2024-07-18 00:24:56 +0530

    arrays

--- arrays/src/com/krish/MultiDimension.java
+++ arrays/src/com/krish/MultiDimension.java
@@ -1,12 +1,28 @@
 package com.krish;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MultiDimension {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int[][] arr = new int[3][3];
         int[][] arr2 = {
                 {1,2,3},
                 {4,5,6},
                 {7,8,9},
         };
+
+        for(int[] a : arr2){
+            System.out.println(Arrays.toString(a));
+        }
+        for(int row = 0; row < arr.length; row++){
+            for (int col = 0; col < arr[row].length; col++){
+                arr[row][col] = input.nextInt();
+            }
+        }
+        for(int[] a : arr){
+            System.out.println(Arrays.toString(a));
+        }
     }
 }
